6e7d99a0403697b136fc3cbba1ddcbc7d107f3af
Mike Parkin <mparkin@gmail.com>
2014-08-16 13:25:28 -0700

    More Coments added.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,15 @@
-## Matrix inversion is usually a costly computation and their may be some benefit 
-## to caching cachethe inverse of a matrix rather than compute it repeatedly 
-## (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
+##------------------------------------------------------------------------------
+##
+## cacheMatrix.R    8/16/14
+## Mike Parkin  mparkin@gmail,com
+##
+## R Functions to cache an inverted matrix. 
+## With large matrices, inverting can take a lot of processing time. 
+## This functions allows the user to store the inverted matrix in a 
+## persistent object. 
+## 
+##-------------------------------------------------------------------------------
+##
 ## functions in this module:
   
 ## makeCacheMatrix:  Creates a special "matrix" object that can cache its inverse.
@@ -11,14 +20,15 @@
 ##    the inverse from the cache.
 
 ## Usage:
-## EX: create a matrix e,g. mymatrix <- matrix(c(4,4,-2,2,6,2,2,8,4),3,3)'
-## create special matrix e.g. myspecialMartix <- makeCacheMatrix(mymatrix)
-## then you can run cacheSolve(myspecialMatrix)
+## EX: create a matrix e,g. myMatrix <- matrix(c(4,4,-2,2,6,2,2,8,4),3,3)'
+## create special matrix e.g. myspecialMatrix <- makeCacheMatrix(myMatrix)
+## then you can run invertedMatrix <- cacheSolve(myspecialMatrix)
 
 
 ## makeCacheMatrix:  Creates a special "matrix" object that can cache its inverse.
 ## this function does not check to make sure the matrix is invertable. If not,
 ## standard R errors will be thrown.
+## Usage: myspecialMatrix <- makeCacheMatrix(mymatrix)
 
 makeCacheMatrix <- function(x = matrix()) {
   ##initialize internal function objects
@@ -27,11 +37,17 @@ makeCacheMatrix <- function(x = matrix()) {
     x <<- y
     m <<- NULL
   }
-  ## define functions getter, setter
+  ## define functions getter
   get <- function() x
+  
+  ## Object Functions to create and retrieve the inverted matrix
+
   ## using the R function "solve() to create inverse matrix
   setinverse <- function(solve) m <<- solve
+  ## retreive cached invert matrix
   getinverse <- function() m
+  
+  ## put object functions in a list to make use/retrevial easier
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -43,13 +59,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ##    makeCacheMatrix above. If the inverse has already been calculated 
 ##   (and the matrix has not changed), then the cachesolve should retrieve 
 ##    the inverse from the cache.
-## this function does not check to make sure the matrix is invertable. If not,
-## standard R errors will be thrown.
+##   this function does not check to make sure the matrix is invertable. If not,
+##   standard R errors will be thrown.
+##USage: invertedMatrix <- cacheSolve(myspecialMatrix)
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
+  ## pull invert matrix from cache
   m <- x$getinverse()
-  ## if inverse matrix already cahaed, return cched value
+  ## if inverse matrix already cached, return cached value
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
@@ -57,8 +75,7 @@ cacheSolve <- function(x, ...) {
   ## else calculate inverse matrix with R solve() function
   data <- x$get()
   m <- solve(data, ...)
-  ## store in cached matrix
+  ## store inverse matrix in cache
   x$setinverse(m)
-  m
-  
+  m  
 }
